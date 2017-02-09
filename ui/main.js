38cb41b596ca0d99ad6d0fc9d5b1f52247773c15
ShubhamChaurasia1997 <shubhamchaurasia535@gmail.com>
2017-02-09 14:44:51 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,17 @@ button.onclick=function(){
     var span = document.getElementById('count');
     span.innerHTML=counter.toString();
 };
+
+
+var nameInput= document.getElementById('name');
+var name= nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick = function(){
+    var names=['name1','name2','name3','name4'];
+    var list='';
+    for(var i=0; i<names.lenght; i++){
+        list+= '<li>' + names[i] + '</li';
+    }
+var ul=getElememtById('namelist');
+    ul.innnerHTML=list;
+};
