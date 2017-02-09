411aaf5eb57b447170dd5200c1b8715a9d0af9a6
ShubhamChaurasia1997 <shubhamchaurasia535@gmail.com>
2017-02-09 13:30:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ button.onclick=function(){
     counter=counter+1;
     var span = document.getElementById('count');
     span.innerHTML=counter.toString();
-}
+};
 
 var nameInput= document.getElementById('name');
 var name= nameInput.value;
