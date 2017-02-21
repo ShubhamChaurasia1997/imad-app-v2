9fa6efdc6aa4c53157f4b70cfd480f98e54fa5a1
ShubhamChaurasia1997 <shubhamchaurasia535@gmail.com>
2017-02-21 14:34:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,13 +24,7 @@ app.get('/counter',function(req,res){
 });
 var pool = new Pool(config);
 app.get('/test-db', function (req, res) {
-    pool.query('SELECT * FROM test',function(err,result){
-        if(err){
-            res.writeHead(500, {'content-type': 'text/plain'});
-        }
-        else{
-            res.send(JSON.stringify(result));
-        }});
+    console.log('Eroor is resolved');
 });
 
 app.get('/ui/style.css', function (req, res) {
